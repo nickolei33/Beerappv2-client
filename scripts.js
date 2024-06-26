ba0bc03c2b3aca34a98c08d229833e035d207fcf
Nicolas <nicolas@MacBook-Air-de-Nicolas.local>
2024-06-26 17:21:11 +0200

    Modification de l'affichage, pour inclure les nouveaux elementsdu back-office et de l'api

--- scripts.js
+++ scripts.js
@@ -1,12 +1,11 @@
 // scripts.js
 document.addEventListener('DOMContentLoaded', () => {
-    // Remplacez cette URL par l'URL réelle de votre API
     const apiURL = 'https://nico-c.info/api/beers';
 
     fetch(apiURL)
         .then(response => response.json())
         .then(data => {
-            const beers = data; // Supposons que la réponse de l'API soit un tableau d'objets bière
+            const beers = data;
             displayBeers(beers);
         })
         .catch(error => console.error('Erreur lors de la récupération des données :', error));
@@ -20,14 +19,30 @@ function displayBeers(beers) {
         beerItem.classList.add('beer-item');
 
         beerItem.innerHTML = `
-            <img src="${beer.image_url}" alt="${beer.name}">
-            <div class="beer-details">
-                <div class="brewery-name">${beer.brewery}</div>
-                <div class="beer-name">${beer.name}</div>
-                <div class="beer-type">${beer.type}  - ABV:${beer.alcohol_percentage}%</div>
-                <div class="beer-price"> 🍷 : ${beer.price_12_5cl}€ - 🍺 : ${beer.price_25cl}€</div>
+            <div class="beer-item-inner">
+                <div class="beer-item-front">
+                    <div class="beer-logo"><img src="${beer.image_url}" alt="Logo de ${beer.name}"></div>
+                    <div class="beer-details">
+                        <div class="brewery-name">${beer.brewery.name} - Stand: ${beer.brewery.stand_number}</div>
+                        <div class="beer-name">${beer.name}</div>
+                        <div class="beer-type">${beer.type}</div>
+                        <div class="beer-price">🍷: ${beer.price_12_5cl}€ - 🍺: ${beer.price_25cl}€</div>
+                    </div>
+                </div>
+                <div class="beer-item-back">
+                    <div class="brewery-logo"><img src="${beer.brewery.logo_url}" alt="Logo de ${beer.brewery.name}"></div>
+                    <div class="beer-details">
+                        <div class="beer-description">${beer.description}</div>
+                        <div class="beer-abv">ABV: ${beer.alcohol_percentage}%</div>
+                        <div class="beer-ibu">IBU: ${beer.ibu}</div>
+                    </div>
+                </div>
             </div>
         `;
+
+        beerItem.addEventListener('click', () => {
+            beerItem.classList.toggle('flip');
+        });
 
         beerList.appendChild(beerItem);
     });
